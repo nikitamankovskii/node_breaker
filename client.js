1d3a60c646f859a7534f4eee2c56d5c0562f2fa1
julien singler <julien.singler@gmail.com>
2018-09-25 17:19:06 +0700

    improved mainly server, just a change on client

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-function send_reqest(url){
+function send_request(url){
   http.get(url,(res) =>{
     let data = '';
     res.on('data', (chunk) => {
@@ -10,10 +10,14 @@ function send_reqest(url){
       console.log(data);
     });
     return data;
+    // added the listener on errors, you could have a problem to reach the server itself, you want to know about it. 
+    // Will save you time during debugging
+  }).on('error', (err) => {
+    console.error("Error: "+ err.message);
   });
 }
 
 const host = 'http://127.0.0.1:8888/'
-console.log(send_reqest(host+'data'))
-console.log(send_reqest(host+'sleep/5'))
-console.log(send_reqest(host+'data'))
+console.log(send_request(host+'data'))
+console.log(send_request(host+'sleep/5'))
+console.log(send_request(host+'data'))
--- server.js
+++ server.js
@@ -3,27 +3,13 @@ var app = express();
 var sleep = require('sleep')
 const port = 8888;
 
-var data = [
-  {
-    id : 1,
-    name : 'name_1'
-  },
-  {
-    id : 2,
-    name : 'name_2'
-  },
-  {
-    id : 3,
-    name : 'name_3'
-  },
-  {
-    id : 4,
-    name : 'name_4'
-  },
-]
+// let's generate ou data array since it contains predictable objects.
+// Software engineers are lazy beast, that's why they are smart, avoid manual tasks as much as possible
+var data = generate_dummy_data(10);
+
 //main page
 app.get('/', function(req, res){
-  res.send('server is runing');
+  res.send('server is running');
 });
 //to see data
 app.get('/data', function(req, res){
@@ -31,10 +17,13 @@ app.get('/data', function(req, res){
 });
 //to get spesific data using 'id'
 app.get('/data/:id', function(req, res){
-  console.log('\n/data/'+req.params.id)
-  var data_user= data.find(function (data){
-    return data.id === Number(req.params.id)
-  })
+  // this is correct, we could have created a data which is an object (like a dict in python) instead of an array
+  // so we wouldn't have to go through the entire array to find our id.
+  // but this is correct since most real world example will be about arrays coming from the database.
+  let data_user = data.find(function (data){
+    // https://stackoverflow.com/questions/4564158/what-is-the-difference-between-parseintstring-and-numberstring-in-javascript
+    return data.id === parseInt(req.params.id) // changed here for ParseInt because it's more flexible to find numbers.
+  });
   res.send(data_user);
 });
 //to get data_user
@@ -43,14 +32,27 @@ app.get('/date', function(req, res){
 });
 // make server sleep
 app.get('/sleep/:seconds', function(req, res){
-  var time_to_sleep = req.params.seconds*1000;
-  msleep(time_to_sleep)
+  let time_to_sleep = parseInt(req.params.seconds)*1000;
+  // no need to redefine msleep since it's inside the "sleep" package you installed
+  sleep.msleep(time_to_sleep)
   res.send('was spleeping - '+time_to_sleep+'ms');
 });
-//sleep function
-function msleep(n) {
-  Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
-}
+
 app.listen(port, () =>{
-  console.log('Server runed on '+port+' on '+Date())
+  console.log('Server run on '+port+' since '+Date())
 });
+
+// this generate an array of dummy objects
+function generate_dummy_data(max){
+  // here we check max is defined and positive.
+  // if not we put a default value of 5
+  let data = [];
+  max = (typeof max === 'undefined' || max <= 0) ? 5 : max;
+  for (let i = 0; i < max; i++) {
+    data.push({
+      id: i,
+      name: 'name_'+i
+    });
+  }
+  return data;
+}
